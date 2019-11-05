625dc34cec35d6369257b50300421d71bff48113
FranckChristmas <francois.noell@gmail.com>
2019-11-05 15:46:13 +0100

    complete rebuild

--- Js/app.js
+++ Js/app.js
@@ -1,5 +1,5 @@
 $('.m-nav-toggle').click(function(e){
 e.preventDefault();
 $('.m-right').toggleClass('is-open'); //permet de déclencher a classe is-open lorsque l'on appuie (le toggle ferme / ouvre le menu via le css translationX 0-100%)
-$('.m-nav-toggle').toggleClass('is-open');
+$('.m-right').toggleClass('is-open');
 })
